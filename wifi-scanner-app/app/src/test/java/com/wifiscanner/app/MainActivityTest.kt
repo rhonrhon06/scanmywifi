8bc089efc66979428b854936908af7b8fca93d54
tomaszlt <tomaszlt@users.noreply.github.com>
2025-06-25 13:56:19 +0000

    Add basic test for MainActivity

---
+++ wifi-scanner-app/app/src/test/java/com/wifiscanner/app/MainActivityTest.kt
@@ -0,0 +1,12 @@
+package com.wifiscanner.app
+
+import org.junit.Assert
+import org.junit.Test
+
+class MainActivityTest {
+    @Test
+    fun testMainActivityCreation() {
+        val activity = MainActivity()
+        Assert.assertNotNull(activity)
+    }
+}
